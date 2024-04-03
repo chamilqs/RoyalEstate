ff6444f288d2e65b8c3172308848519cd9edb5ff
cristofer001 <cristoferdelamota0105@gmail.com>
2024-04-02 21:08:20 -0400

    [FIX]: Configure new properties to entities

--- RoyalState.Core.Domain/Entities/Agent.cs
+++ RoyalState.Core.Domain/Entities/Agent.cs
@@ -5,6 +5,8 @@ namespace RoyalState.Core.Domain.Entities
     public class Agent : BaseEntity
     {
         public string UserId { get; set; }
+        public string ImageUrl { get; set; }
+
         // Navigation Properties
         public ICollection<Property>? Properties { get; set; }
     }
