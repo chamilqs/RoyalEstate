727aa55383d7730ff075676425238db0168dba26
cristofer001 <cristoferdelamota0105@gmail.com>
2024-04-03 22:01:10 -0400

    [New]: Add all SalesType core bussiness

---
+++ RoyalState.Core.Application/Interfaces/Services/ISalesTypeService.cs
@@ -0,0 +1,9 @@
+﻿using RoyalState.Core.Application.ViewModels.SalesTypes;
+using RoyalState.Core.Domain.Entities;
+
+namespace RoyalState.Core.Application.Interfaces.Services
+{
+    public interface ISalesTypeService : IGenericService<SaveSalesTypeViewModel, SalesTypeViewModel, SaleType>
+    {
+    }
+}
---
+++ RoyalState.Core.Application/ViewModels/SalesTypes/SalesTypeViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace RoyalState.Core.Application.ViewModels.SalesTypes
+{
+    public class SalesTypeViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<PropertyViewModel>? Properties { get; set; }
+    }
+}
