1d248e137c2677a20aa9462d9788350d0878a57d
cristofer001 <cristoferdelamota0105@gmail.com>
2024-04-06 11:51:54 -0400

    [NEW]: Add Type mapping

---
+++ RoyalState.Core.Application/Mappings/TypeProfile.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using RoyalState.Core.Application.ViewModels.PropertyTypes;
+using RoyalState.Core.Application.ViewModels.SaleTypes;
+using RoyalState.Core.Domain.Entities;
+
+namespace RoyalState.Core.Application.Mappings
+{
+    public class TypeProfile : Profile
+    {
+        public TypeProfile()
+        {
+            #region SaleType
+            CreateMap<SaleType, SaleTypeViewModel>()
+                .ForMember(dest => dest.PropertiesQuantity, option => option.Ignore())
+                .ReverseMap();
+
+            CreateMap<SaleType, SaveSaleTypeViewModel>()
+                .ReverseMap()
+                .ForMember(origin => origin.Properties, option => option.Ignore());
+            #endregion
+
+            #region PropertyType
+            CreateMap<PropertyType, PropertyTypeViewModel>()
+                .ForMember(dest => dest.PropertiesQuantity, option => option.Ignore())
+                .ReverseMap();
+
+            CreateMap<PropertyType, SavePropertyTypeViewModel>()
+                .ReverseMap()
+                .ForMember(origin => origin.Properties, option => option.Ignore());
+            #endregion
+        }
+    }
+}
--- RoyalState.Core.Domain/Entities/Improvement.cs
+++ RoyalState.Core.Domain/Entities/Improvement.cs
@@ -5,7 +5,6 @@ namespace RoyalState.Core.Domain.Entities
     public class Improvement : TypeBaseEntity
     {
         // Navigation Properties
-
         public ICollection<PropertyImprovement>? PropertyImprovements { get; set; }
     }
 }
