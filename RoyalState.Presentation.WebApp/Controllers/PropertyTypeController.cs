cda321d117219b4275bc1d1e5fab57f002d497a2
cristofer001 <cristoferdelamota0105@gmail.com>
2024-04-09 19:15:17 -0400

    [NEW]: Add PropertyType controller

---
+++ RoyalState.Presentation.WebApp/Controllers/PropertyTypeController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RoyalState.Core.Application.Interfaces.Services;
+using RoyalState.Core.Application.ViewModels.PropertyTypes;
+
+namespace RoyalState.Presentation.WebApp.Controllers
+{
+    //[Authorize(Roles = "Admin")]
+    public class PropertyTypeController : Controller
+    {
+        private readonly IPropertyTypeService _propertyTypeService;
+
+        public PropertyTypeController(IPropertyTypeService propertyTypeService)
+        {
+            _propertyTypeService = propertyTypeService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _propertyTypeService.GetAllViewModel());
+        }
+
+        #region Create
+        public async Task<IActionResult> Create()
+        {
+            return View("SavePropertyType", new SavePropertyTypeViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(SavePropertyTypeViewModel vm)
+        {
+            if (!ModelState.IsValid)
+                return View("SavePropertyType", vm);
+
+            await _propertyTypeService.Add(vm);
+
+            return RedirectToRoute(new { controller = "PropertyType", action = "Index" });
+        }
+        #endregion
+
+        #region Edit
+        public async Task<IActionResult> Edit(int id)
+        {
+            SavePropertyTypeViewModel vm = await _propertyTypeService.GetByIdSaveViewModel(id);
+
+            return View("SavePropertyType", vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(SavePropertyTypeViewModel vm)
+        {
+            if (!ModelState.IsValid)
+                return View("SavePropertyType", vm);
+
+            await _propertyTypeService.Update(vm, vm.Id);
+
+            return RedirectToRoute(new { controller = "PropertyType", action = "Index" });
+        }
+        #endregion
+
+        #region Delete
+        public async Task<IActionResult> Delete(int id)
+        {
+            return View(await _propertyTypeService.GetByIdViewModel(id));
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public async Task<IActionResult> DeletePost(int id)
+        {
+            await _propertyTypeService.Delete(id);
+
+            return RedirectToRoute(new { controller = "PropertyType", action = "Index" });
+        }
+        #endregion
+    }
+}
