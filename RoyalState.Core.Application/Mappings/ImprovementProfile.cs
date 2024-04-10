49b327c6d7d7c7a2949795bcbfb36f4001216a86
cristofer001 <cristoferdelamota0105@gmail.com>
2024-04-09 20:31:20 -0400

    [FIX]: Update little things

---
+++ RoyalState.Core.Application/Mappings/ImprovementProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using RoyalState.Core.Application.ViewModels.Improvements;
+using RoyalState.Core.Domain.Entities;
+
+namespace RoyalState.Core.Application.Mappings
+{
+    public class ImprovementProfile : Profile
+    {
+        public ImprovementProfile()
+        {
+            #region Improvement
+            CreateMap<Improvement, ImprovementViewModel>()
+                .ReverseMap();
+
+            CreateMap<Improvement, SaveImprovementViewModel>()
+                .ReverseMap();
+            #endregion
+        }
+    }
+}
