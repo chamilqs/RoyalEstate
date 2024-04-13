612ed532bed40937b0abf7299cd4ba94932137e0
Samil <114354322+chamilqs@users.noreply.github.com>
2024-04-12 21:38:56 -0400

    [UPDATE] -->Added the GetAgentProperties that retrieves all the properties from an specified agent.

--- RoyalState.Core.Application/Interfaces/Services/IPropertyService.cs
+++ RoyalState.Core.Application/Interfaces/Services/IPropertyService.cs
@@ -6,6 +6,7 @@ namespace RoyalState.Core.Application.Interfaces.Services
     public interface IPropertyService : IGenericService<SavePropertyViewModel, PropertyViewModel, Property>
     {
         Task<PropertyViewModel> GetAllViewModelWIthFilters(FilterPropertyViewModel filterProperty);
+        Task<List<PropertyViewModel>> GetAgentProperties(int id);
         Task<PropertyViewModel> GetPropertyByCode(string code);
     }
 }
