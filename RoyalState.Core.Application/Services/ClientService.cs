6c2128ebea43beec1d26284b5ac6a31f37578dd5
Samil <114354322+chamilqs@users.noreply.github.com>
2024-04-13 19:47:43 -0400

    [NEW] --> Updated features for the client, now can add properties as favorites and delete them.

--- RoyalState.Core.Application/Services/ClientService.cs
+++ RoyalState.Core.Application/Services/ClientService.cs
@@ -7,30 +7,41 @@ using RoyalState.Core.Application.ViewModels.Client;
 using RoyalState.Core.Application.ViewModels.Users;
 using RoyalState.Core.Domain.Entities;
 using Microsoft.AspNetCore.Http;
+using RoyalState.Core.Application.ViewModels.ClientProperties;
+using RoyalState.Core.Application.ViewModels.Property;
 
 namespace RoyalState.Core.Application.Services
 {
+    #region ClientService
     public class ClientService : GenericService<SaveClientViewModel, ClientViewModel, Client>, IClientService
     {
+        #region Fields
         private readonly IClientRepository _clientRepository;
+        private readonly IClientPropertiesService _clientPropertiesService;
+        private readonly IPropertyService _propertiesService;
         private readonly IUserService _userService;
         private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly AuthenticationResponse user;
         private readonly IMapper _mapper;
+        #endregion
 
-        public ClientService(IClientRepository clientRepository, IHttpContextAccessor httpContextAccessor, IMapper mapper, IUserService userService) : base(clientRepository, mapper)
+        #region Constructor
+        public ClientService(IClientRepository clientRepository, IClientPropertiesService clientPropertiesService, IUserService userService, IHttpContextAccessor httpContextAccessor, IMapper mapper, IPropertyService propertiesService) : base(clientRepository, mapper)
         {
-            _httpContextAccessor = httpContextAccessor;
             _clientRepository = clientRepository;
-            _mapper = mapper;
-            user = _httpContextAccessor.HttpContext.Session.Get<AuthenticationResponse>("user");
+            _clientPropertiesService = clientPropertiesService;
             _userService = userService;
+            _mapper = mapper;
+            _httpContextAccessor = httpContextAccessor;
+            user = _httpContextAccessor.HttpContext.Session.Get<AuthenticationResponse>("user");
+            _propertiesService = propertiesService;
         }
+        #endregion
 
         #region GetByUserIdViewModel
         public async Task<ClientViewModel> GetByUserIdViewModel(string userId)
         {
-            var clientList = await base.GetAllViewModel();
+            var clientList = await GetAllViewModel();
             ClientViewModel client = clientList.FirstOrDefault(client => client.UserId == userId);
 
             return client;
@@ -61,11 +72,65 @@ namespace RoyalState.Core.Application.Services
         #endregion
 
         #region MarkPropertyAsFavorite
+        public async Task<SaveClientPropertiesViewModel> MarkPropertyAsFavorite(SaveClientPropertiesViewModel vm)
+        {
+            await _clientPropertiesService.Add(vm);
 
+            return vm;
+        }
+
+        #endregion
+
+        #region DeleteFavorite
+        public async Task DeleteFavorite(int id)
+        {
+            var clientProperties = await _clientPropertiesService.GetByPropertyIdViewModel(id);
+
+            await _clientPropertiesService.Delete(clientProperties.Id);
+        }
+        #endregion
+
+        #region GetFavoriteProperties
+        public async Task<List<int>> GetIdsOfFavoriteProperties(int id)
+        {
+            var clientProperties = await _clientPropertiesService.GetAllViewModel();
+
+
+            List<int> properties = new List<int>();
+            foreach (var property in clientProperties)
+            {
+                if (property.ClientId == id)
+                {
+                    properties.Add(property.PropertyId);
+                }
+
+            }
+
+            return properties;
+        }
+        #endregion
+
+        #region GetFavoritePropertiesViewModel
+        public async Task<List<PropertyViewModel>> GetFavoritePropertiesViewModel(int id)
+        {
+            var properties = await _propertiesService.GetAllViewModel();
+            var clientProperties = await _clientPropertiesService.GetAllViewModel();
+
+            var favoritePropertyIds = clientProperties
+                .Where(client => client.ClientId == id)
+                .Select(client => client.PropertyId);
+
+            List<PropertyViewModel> favoriteProperties = properties
+                .Where(property => favoritePropertyIds.Contains(property.Id))
+                .ToList();
+
+            return favoriteProperties;
+        }
 
 
         #endregion
 
 
     }
+    #endregion
 }
--- RoyalState.Presentation.WebApp/Controllers/ClientController.cs
+++ RoyalState.Presentation.WebApp/Controllers/ClientController.cs
@@ -2,12 +2,14 @@
 using RoyalState.Core.Application.DTOs.Account;
 using RoyalState.Core.Application.Helpers;
 using RoyalState.Core.Application.Interfaces.Services;
+using RoyalState.Core.Application.ViewModels.ClientProperties;
 
 namespace RoyalState.Presentation.WebApp.Controllers
 {
     public class ClientController : Controller
     {
         private readonly IClientService _clientService;
+        private readonly IAccountService _accountService;
         private readonly IPropertyService _propertyService;
         private readonly IFileService _fileService;
         private readonly IHttpContextAccessor _httpContextAccessor;
@@ -22,20 +24,61 @@ namespace RoyalState.Presentation.WebApp.Controllers
 
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string? error)
         {
+            if (error != null)
+            {
+                ViewBag.Error = error;
+            }
             var properties = await _propertyService.GetAllViewModel();
-
+            return View(properties);
+        }
+        public async Task<IActionResult> MyFavorites()
+        {
+            var client = await _clientService.GetByUserIdViewModel(authViewModel.Id);
+            var properties = await _clientService.GetFavoritePropertiesViewModel(client.Id);
             return View(properties);
         }
 
         [HttpPost]
-        public async Task<IActionResult> MarkPropertyAsFavorite()
+        public async Task<IActionResult> MarkPropertyAsFavorite(int propertyId)
         {
+            var client = await _clientService.GetByUserIdViewModel(authViewModel.Id);
+            var clientFavoritePropertyIds = await _clientService.GetIdsOfFavoriteProperties(client.Id);
 
-            return View();
+            if (!clientFavoritePropertyIds.Contains(propertyId))
+            {
+                var clientProperty = new SaveClientPropertiesViewModel
+                {
+                    ClientId = client.Id,
+                    PropertyId = propertyId
+                };
+
+                await _clientService.MarkPropertyAsFavorite(clientProperty);
+            }
+            else
+            {
+                var error = "This property is already in your favorites";
+                return RedirectToAction("Index", "Client", new { error = error });
+            }
+
+            return RedirectToAction("Index");
         }
 
+        public async Task<IActionResult> DeleteFavorite(int id)
+        {
+            var property = await _propertyService.GetByIdSaveViewModel(id);
+
+            return View(property);
+        }        
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteFavoritePost(int id)
+        {
+
+            await _clientService.DeleteFavorite(id);
+            return RedirectToAction("MyFavorites");
+        }
 
 
     }
