484ff9c4888d69b2995e158711d415356b3aa881
Samil <114354322+chamilqs@users.noreply.github.com>
2024-04-13 17:09:25 -0400

    [NEW] [UPDATE] --> Added a method to the HomeController to filter a property by its code.

--- RoyalState.Presentation.WebApp/Controllers/HomeController.cs
+++ RoyalState.Presentation.WebApp/Controllers/HomeController.cs
@@ -1,23 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using RoyalState.Core.Application.Helpers;
 using RoyalState.Core.Application.DTOs.Account;
 using RoyalState.Core.Application.Enums;
+using RoyalState.Core.Application.Helpers;
+using RoyalState.Core.Application.Interfaces.Services;
+using RoyalState.Core.Application.ViewModels.Property;
 
 namespace RoyalState.Presentation.WebApp.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IPropertyService _propertyService;
         private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly AuthenticationResponse authViewModel;
 
-        public HomeController(IHttpContextAccessor httpContextAccessor)
+        public HomeController(IHttpContextAccessor httpContextAccessor, IPropertyService propertyService)
         {
             _httpContextAccessor = httpContextAccessor;
+            _propertyService = propertyService;
             authViewModel = _httpContextAccessor.HttpContext.Session.Get<AuthenticationResponse>("user");
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var properties = await _propertyService.GetAllViewModel();
+
             if (authViewModel != null)
             {
                 if (authViewModel.Roles.Any(role => role == Roles.Admin.ToString()))
@@ -34,8 +40,28 @@ namespace RoyalState.Presentation.WebApp.Controllers
                 }
             }
 
-            return View();
+            return View(properties);
         }
+
+        #region Search
+        public async Task<IActionResult> SearchProperty(string code)
+        {
+            var property = await _propertyService.GetPropertyByCode(code);
+            bool isEmpty = property == null;
+
+            ViewBag.IsEmpty = isEmpty;
+
+            List<PropertyViewModel> properties = new();
+            if (!isEmpty)
+            {
+                properties.Add(property);
+            }
+
+            return View("Index", property != null ? properties : new List<PropertyViewModel>());
+
+        }
+        #endregion
+
 
     }
 }
