c6d809e5d25d305270011521e08f6f6cc45dac3b
cristofer001 <cristoferdelamota0105@gmail.com>
2024-04-14 10:27:02 -0400

    [NEW]: Add admin controller

--- RoyalState.Presentation.WebApp/Controllers/AdminController.cs
+++ RoyalState.Presentation.WebApp/Controllers/AdminController.cs
@@ -4,6 +4,7 @@ using RoyalState.Core.Application.DTOs.Account;
 using RoyalState.Core.Application.Enums;
 using RoyalState.Core.Application.Helpers;
 using RoyalState.Core.Application.Interfaces.Services;
+using RoyalState.Core.Application.Services;
 using RoyalState.Core.Application.ViewModels.Admins;
 
 namespace RoyalState.Presentation.WebApp.Controllers
@@ -27,6 +28,59 @@ namespace RoyalState.Presentation.WebApp.Controllers
             return View();
         }
 
+        #region Create
+        public async Task<IActionResult> Create()
+        {
+            return View("SaveAdmin", new SaveAdminViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(SaveAdminViewModel vm)
+        {
+            if (!ModelState.IsValid)
+                return View("SaveAdmin", vm);
+
+            await _adminService.Add(vm);
+
+            return RedirectToRoute(new { controller = "Admin", action = "Index" });
+        }
+        #endregion
+
+        #region Edit
+        public async Task<IActionResult> Edit(int id)
+        {
+            SaveAdminViewModel vm = await _adminService.GetByIdSaveViewModel(id);
+
+            return View("SaveAdmin", vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(SaveAdminViewModel vm)
+        {
+            if (!ModelState.IsValid)
+                return View("SaveAdmin", vm);
+
+            await _adminService.Update(vm, vm.Id);
+
+            return RedirectToRoute(new { controller = "Admin", action = "Index" });
+        }
+        #endregion
+
+        #region Delete
+        public async Task<IActionResult> Delete(int id)
+        {
+            return View(await _adminService.GetByIdViewModel(id));
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public async Task<IActionResult> DeletePost(int id)
+        {
+            await _adminService.Delete(id);
+
+            return RedirectToRoute(new { controller = "Admin", action = "Index" });
+        }
+        #endregion
 
 
         #region Active & Unactive User
