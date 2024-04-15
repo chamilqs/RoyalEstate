23d63b39feac8037cabf35528d19930eb18ae7fd
cristofer001 <cristoferdelamota0105@gmail.com>
2024-04-15 07:17:01 -0400

    [NEW]: Add developer's maintenance

---
+++ RoyalState.Core.Application/Mappings/DeveloperProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using RoyalState.Core.Application.ViewModels.Developers;
+using RoyalState.Core.Domain.Entities;
+
+namespace RoyalState.Core.Application.Mappings
+{
+    public class DeveloperProfile : Profile
+    {
+        public DeveloperProfile()
+        {
+            #region Developer
+            CreateMap<Developer, DeveloperViewModel>()
+                .ForMember(dest => dest.Username, option => option.Ignore())
+                .ForMember(dest => dest.FirstName, option => option.Ignore())
+                .ForMember(dest => dest.LastName, option => option.Ignore())
+                .ForMember(dest => dest.Email, option => option.Ignore())
+                .ForMember(dest => dest.EmailConfirmed, option => option.Ignore())
+                .ReverseMap();
+
+            CreateMap<Developer, SaveDeveloperViewModel>()
+                .ReverseMap()
+                .ForMember(origin => origin.LastModifiedBy, option => option.Ignore())
+                .ForMember(origin => origin.LastModifiedDate, option => option.Ignore());
+            #endregion
+        }
+    }
+}
---
+++ RoyalState.Core.Application/ViewModels/Developers/SaveDeveloperViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace RoyalState.Core.Application.ViewModels.Developers
+{
+    public class SaveDeveloperViewModel
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string Identification { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
